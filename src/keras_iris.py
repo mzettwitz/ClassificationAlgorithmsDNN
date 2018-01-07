e08c94b808199aa9ef00a3c5ced115162f7df48c
Martin Zettwitz <martin.zettwitz@st.ovgu.de>
2018-01-07 15:06:21 +0100

    minors

--- src/keras_iris.py
+++ src/keras_iris.py
@@ -1,18 +1,13 @@
 ##################################
-#imports
+# IMPORTS
 ##################################
 import numpy
-import pandas
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
-from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import KFold
 from matplotlib import pyplot
 from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
 from time import time as time
 import arff
 
